459286f42d77f25777c5394ceae123d0b3363119
fkskdldh@naver.com <fkskdldh@naver.com>
2025-02-28 20:27:45 +0900

    git inti

---
+++ src/main/java/com/service/RSIranking/RsIrankingApplication.java
@@ -0,0 +1,13 @@
+package com.service.RSIranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsIrankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsIrankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/RSIranking/RsIrankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.RSIranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsIrankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

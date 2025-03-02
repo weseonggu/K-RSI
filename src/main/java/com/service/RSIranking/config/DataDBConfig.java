107ec57f76cc50d17cd7a3931acc40e1696d085a
fkskdldh@naver.com <fkskdldh@naver.com>
2025-03-02 19:37:32 +0900

    setting DB연결 설정 클래스

---
+++ src/main/java/com/service/RSIranking/config/DataDBConfig.java
@@ -0,0 +1,57 @@
+package com.service.RSIranking.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.service.RSIranking.repository",
+        entityManagerFactoryRef = "dataEntityManager",
+        transactionManagerRef = "dataTransactionManager"
+)
+public class DataDBConfig {
+
+    @Bean(name = "dataDBSource")
+    @ConfigurationProperties(prefix = "spring.datasource-data")
+    public DataSource dataDBSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "dataEntityManager")
+    public LocalContainerEntityManagerFactoryBean dataEntityManager(
+            @Qualifier("dataDBSource") DataSource dataSource) {
+
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.service.RSIranking.entity"); // jpa 엔티티 패키지
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        em.setJpaPropertyMap(properties);
+
+        return em;
+    }
+
+    @Bean(name = "dataTransactionManager")
+    public PlatformTransactionManager dataTransactionManager(
+            @Qualifier("dataEntityManager") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+
+        return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+}

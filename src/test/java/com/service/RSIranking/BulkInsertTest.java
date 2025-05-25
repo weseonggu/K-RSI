aad4e6f842cc610b5399d924740ecf4037c089d9
fkskdldh@naver.com <fkskdldh@naver.com>
2025-05-25 20:33:40 +0900

    날짜가 휴장일인지 알아내는 클래스 구현 완료

--- src/test/java/com/service/RSIranking/BulkInsertTest.java
+++ src/test/java/com/service/RSIranking/BulkInsertTest.java
@@ -1,16 +1,13 @@
 package com.service.RSIranking;
 
-import com.service.RSIranking.entity.SecuritiesStockEntity;
 import com.service.RSIranking.repository.jdbc.SecuritiesStockJDBCRepository;
 import com.service.RSIranking.service.StockBulkInsertService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-
 @SpringBootTest
+@Disabled
 public class BulkInsertTest {
     @Autowired
     private StockBulkInsertService stockBulkInsertService;
@@ -18,18 +15,18 @@ public class BulkInsertTest {
     @Autowired
     private SecuritiesStockJDBCRepository securitiesStockJDBCRepository;
 
-    @Test
-    void bulkInsertRollbackTest() {
-        // 예제 데이터 생성
-        List<SecuritiesStockEntity> testStocks = List.of(
-                new SecuritiesStockEntity("000006", "Stock A", "KOSPI", true),
-                new SecuritiesStockEntity("000002", "Stock B", "KOSDAQ", true),
-                new SecuritiesStockEntity("000001", "Stock C", "KOSPI", true)
-        );
-
-        // 예외 발생을 기대하면서 Bulk Insert 실행
-        Assertions.assertThrows(RuntimeException.class, () -> {
-            stockBulkInsertService.stocksInsert(testStocks);
-        });
-    }
+//    @Test
+//    void bulkInsertRollbackTest() {
+//        // 예제 데이터 생성
+//        List<SecuritiesStockEntity> testStocks = List.of(
+//                new SecuritiesStockEntity("000006", "Stock A", "KOSPI", true),
+//                new SecuritiesStockEntity("000002", "Stock B", "KOSDAQ", true),
+//                new SecuritiesStockEntity("000001", "Stock C", "KOSPI", true)
+//        );
+//
+//        // 예외 발생을 기대하면서 Bulk Insert 실행
+//        Assertions.assertThrows(RuntimeException.class, () -> {
+//            stockBulkInsertService.stocksInsert(testStocks);
+//        });
+//    }
 }

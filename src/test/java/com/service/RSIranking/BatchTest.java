36a6274027d999792488692070acb63348829908
fkskdldh@naver.com <fkskdldh@naver.com>
2025-06-03 20:59:25 +0900

    종목 업제이트 밑 주식 매매 정보 업데이트 테스트

--- src/test/java/com/service/RSIranking/BatchTest.java
+++ src/test/java/com/service/RSIranking/BatchTest.java
@@ -1,5 +1,6 @@
 package com.service.RSIranking;
 
+import com.service.RSIranking.schedule.DailyTradingInfoLauncher;
 import com.service.RSIranking.schedule.StockInfoLauncher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,50 @@ public class BatchTest {
             System.out.println(e.getMessage());
         }
 
+    }
+
+    @Autowired
+    private DailyTradingInfoLauncher dailyTradingInfoLauncher;
+
+    @Test
+    public void tradingIngoUpdateTest(){
+        Integer marketDay[] = {
+                20250602,
+                20250530,
+                20250529,
+                20250528,
+                20250527,
+                20250526,
+                20250523,
+                20250522,
+                20250521,
+                20250520,
+                20250519,
+                20250516,
+                20250515,
+                20250514,
+                20250513,
+                20250512,
+                20250509,
+                20250508,
+                20250507,
+                20250502,
+                20250501,
+                20250430,
+                20250429,
+                20250428,
+                20250425,
+                20250424,
+                20250423,
+                20250422};
+        try{
+            for(Integer date: marketDay){
+                dailyTradingInfoLauncher.dailyTradingInfoJobLauncher(date.toString());
+                Thread.sleep(5000);
+            }
+        }catch (Exception e){
+
+        }
     }
 
 }

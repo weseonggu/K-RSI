fdc84f6b292c3fb3057d6e2c0dd65b825fcbd118
DESKTOP-6L2Q5A5\WeSeongGu <weseonggu>
2025-07-08 20:43:27 +0900

    매매 정보 비동기 병렬실행 적용 사이즈:100

--- src/main/java/com/service/RSIranking/service/UpdateDailyTradingInfoService.java
+++ src/main/java/com/service/RSIranking/service/UpdateDailyTradingInfoService.java
@@ -5,11 +5,13 @@ import com.service.RSIranking.entity.DailyTradingInformation;
 import com.service.RSIranking.repository.jdbc.DailyTradingInformationJDBCRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
@@ -23,15 +25,17 @@ public class UpdateDailyTradingInfoService {
      * @param tradingInfoDtos 일별 매매정보
      */
     @Transactional
-    public void tradingInfoInsert(List<DailyTradingInformation> tradingInfoDtos, List<TradingInfoDto> baseDto) throws Exception {
+    @Async("dailtTrandingExecutor")
+    public CompletableFuture<Void> tradingInfoInsert(List<DailyTradingInformation> tradingInfoDtos, List<TradingInfoDto> baseDto) throws Exception {
         try{
             dailyTradingInformationJDBCRepository.bulkInsert(tradingInfoDtos, baseDto);
+            return CompletableFuture.completedFuture(null);
         }catch (SQLIntegrityConstraintViolationException e){
             log.info("이미 저장된 데이터 입니다.");
-            throw e;
+            return CompletableFuture.failedFuture(e);
         }
         catch (Exception e){
-            throw e;
+            return CompletableFuture.failedFuture(e);
         }
     }
 }
